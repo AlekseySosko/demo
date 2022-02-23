8dfb0c8b48002d035926c9e618904beb8defacb7
Sosko Aleksey <sosko.aleksey@gmail.com>
2022-02-23 21:53:08 +0700

    ef

--- Index.java
+++ Index.java
@@ -1,5 +1,6 @@
 class Index {
     public static void main(String[] args) {
-        System.out.println("Demo!!");   
+        System.out.println("Demo!!");
+        System.out.println("Ещё!"); 
     }
 }
